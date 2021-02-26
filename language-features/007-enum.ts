04dd116e0c8763eb4909e0b60f29d1c1af000cc9
Nils Knappmeier <nils.knappmier@cosee.biz>
2021-02-26 09:05:15 +0100

    add example for why enums are not so great

--- language-features/007-enum.ts
+++ language-features/007-enum.ts
@@ -1,30 +1,45 @@
 // An enum type is actually an object.  This enum
 export enum Status {
-    "OK","Warning","Critical"
+  "OK",
+  "Warning",
+  "Critical",
 }
 // is equivalent to
 
 const StatusObj = {
-    "OK": 0,
-    "Warning": 1,
-    "Critical": 2,
+  OK: 0,
+  Warning: 1,
+  Critical: 2,
+};
 
-}
-
-console.log(Status.OK) // 0
-console.log(Status.Warning) // 1
-console.log(Status[Status.OK]) // "OK"
+console.log(Status.OK); // 0
+console.log(Status.Warning); // 1
+console.log(Status[Status.OK]); // "OK"
 
 // we have had many problems with enums in multiple projects and decided to
 // use constant union types like instead:
 
-type StatusType = 'OK' | 'Warning' | 'Critical'
+type StatusType = "OK" | "Warning" | "Critical";
 
 // or
 
-const StatusValues = ['OK','Warning','Critical'] as const
-type StatusType2 = typeof StatusValues[number]
+const StatusValues = ["OK", "Warning", "Critical"] as const;
+type StatusType2 = typeof StatusValues[number];
 
 // This is clearer and does not imply a mapping.
 
-export {}
+
+// This is an example for when enums don't work so well
+export enum Mapping {
+  MyA = "A",
+  MyB = "B",
+}
+
+function doSomething(param: Mapping): void {
+
+}
+
+// This does not work, even though "Mapping.MyA" equals "A"
+// doSomething("A");
+
+export {};
